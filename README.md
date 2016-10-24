1bb3eb74ce450595ed8fd4316561750b7f882296
Ramadani <email.ramadani@gmail.com>
2016-10-24 17:08:13 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Catagger
+
+Create a collection of types
+
+## About ReduStudio
+
+[ReduStudio][homepage] is web development freelancers based in Yogyakarta and East Borneo, Indonesia. We specialise in developing websites and web apps with Laravel, the most popular PHP Framework.
+
+### Let's Start Project With Us
+
+Just Contact Us At:
+- Email: [redustudio@gmail.com][mailto]
+- Facebook: [ReduStudio's FB Page][fbpage]
+
+## License
+The [MIT][mitlink] License (MIT). Please see [License File](LICENSE.md) for more information.
+
+
+[screenshot]: admin.png
+[homepage]: http://redustudio.com/
+[mailto]: mailto:redustudio@gmail.com
+[fbpage]: https://www.facebook.com/Redustudio/
+[mitlink]: http://opensource.org/licenses/MIT
---
+++ src/ServiceProvider.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Redustudio\Catagger;
+
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+
+/**
+ * Class PackageServiceProvider
+ *
+ * @package Redustudio\Catagger
+ */
+class ServiceProvider extends BaseServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Application is booting
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerMigrations();
+        $this->registerConfigurations();
+    }
+
+    /**
+     * Register the package migrations
+     *
+     * @return void
+     */
+    protected function registerMigrations()
+    {
+        $this->loadMigrationsFrom($this->packagePath('database/migrations'));
+
+        $this->publishes([
+            $this->packagePath('database/migrations') => database_path('/migrations')
+        ], 'migrations');
+    }
+
+    /**
+     * Register the package configurations
+     *
+     * @return void
+     */
+    protected function registerConfigurations()
+    {
+        $this->mergeConfigFrom(
+            $this->packagePath('config/config.php'), 'redustudio.catagger'
+        );
+        $this->publishes([
+            $this->packagePath('config/config.php') => config_path('redustudio/catagger.php'),
+        ], 'config');
+    }
+
+    /**
+     * Loads a path relative to the package base directory
+     *
+     * @param string $path
+     * @return string
+     */
+    protected function packagePath($path = '')
+    {
+        return sprintf("%s/../%s", __DIR__ , $path);
+    }
+}
